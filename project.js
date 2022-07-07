995606463443a206ad402a4e15856716a8d030da
Joel Ajiola <ajiolajoel@gmail.com>
2022-07-07 06:15:09 +0100

    the update

--- project.js
+++ project.js
@@ -1,3 +1,34 @@
-const age = 'ayelowo';
-console.log(`${age}`.toLowerCase());
 
+const label1 = document.querySelector('.label__1')
+const label2 = document.querySelector('.label__2')
+const label3 = document.querySelector('.label__3')
+const label4 = document.querySelector('.label__4')
+const label5 = document.querySelector('.label__5')
+
+const inputDay1 = document.querySelector('.input__1')
+const inputDay2 = document.querySelector('.input__2')
+const inputDay3 = document.querySelector('.input__3')
+const inputDay4 = document.querySelector('.input__4')
+const inputDay5 = document.querySelector('.input__5')
+
+const submit1 = document.querySelector('.submit__1')
+const submit2 = document.querySelector('.submit__2')
+const submit3 = document.querySelector('.submit__3')
+const submit4 = document.querySelector('.submit__4')
+const submit5 = document.querySelector('.submit__5')
+
+function handleClick1(submit__1) {
+  submit__1.innerHTML= alert("Studying");
+}
+function handleClick2(submit__2) {
+  submit__2.innerHTML= alert("Typing");
+}
+function handleClick3(submit__3) {
+  submit__3.innerHTML= alert("Strolling");
+}
+function handleClick4(submit__4) {
+  submit__4.innerHTML= alert("Reading");
+}
+function handleClick5(submit__5) {
+  submit__5.innerHTML= alert("Surfing");
+}
