a662c21bc2b8823d9667423c5e823a56be79b170
Joel Ajiola <ajiolajoel@gmail.com>
2022-07-10 14:22:53 +0100

    project development

---
+++ project2.js
@@ -0,0 +1,18 @@
+function HandleEventLoad(element) {
+   element.innerHTML = alert("welcome!!!");
+}
+function HandleMouseOver(Element) {
+    Element.innerHTML = prompt("Do you subscribe to our Insurance Scheme?");
+}
+function HandleMouseOver2(Element2) {
+    Element2.innerHTML = prompt("Do you subscribe to our Investment services?");
+
+}
+function Handleclick(Element3) {
+    Element3.innerHTML = alert("Our online marketing mode")
+    
+}
+function handleclick2(Element4) {
+    Element4.innerHTML = alert("your opinion has been noted")
+    
+}
