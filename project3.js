4cbc3587ce4ef8aa3952046ee7fb265bdccdb94a
Joel Ajiola <ajiolajoel@gmail.com>
2022-09-07 09:10:04 +0100

    project dev

--- project3.js
+++ project3.js
@@ -1,3 +1,48 @@
-function handleclickcontent(element2) {
-    element2.style("color:red;")
+function HandleLoad(bodyload) {
+    bodyload.innerHTML = alert("welcome to amigold")
+}
+function handleclick(button) {
+    button.style="background-color:red; height:50px; width:130px; color:blue; font-family:Georgia; font-size:20px; border-radius:5px;"
+}
+function mouseover(cashvalue) {
+    cashvalue.style="color:brown;"
+}
+function mouseover2(cashhousing) {
+    cashhousing.style="color:brown;"
+}
+function mouseover3(cashgrowth) {
+    cashgrowth.style="color:brown;"
+}
+function handlesubmit(submitopinion) {
+    submitopinion.innerHTML=alert("your opinon counts!")
+}
+function handlesearch(search) {
+    search.style="background-color:burlywood; height:30px; width:180px; border-radius:7px;"
+}
+function handlesubmitform(submitform) {
+    submitform.innerHTML=alert("your form has been submitted!")
+}
+function handleinput(input) {
+    input.style="background-color:blue; height:25px; width:200px; border-top-left-radius:7px;"
+}
+function handleinput2(input2) {
+    input2.style="background-color:blue; height:25px; width:200px; border-top-left-radius:7px;"
+}
+function handleinput3(input3) {
+    input3.style="background-color:blue; height:25px; width:200px; border-top-left-radius:7px;"
+}
+function handleinput4(input4) {
+    input4.style="background-color:blue; height:40px; width:400px; border-top-left-radius:7px;"
+}
+function handleinput5(input5) {
+    input5.style="background-color:blue; height:25px; width:200px; border-top-left-radius:7px;"
+}
+function handleinput6(input6) {
+    input6.style="background-color:blue; height:25px; width:200px; border-top-left-radius:7px;"
+}
+function handlelink(link1) {
+    link1.style="text-decoration:underline; color:brown;"
+}
+function handlelink2(link2) {
+    link2.style="text-decoration:underline; color:brown;"
 }
